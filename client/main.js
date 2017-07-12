c2660309b4f49e56be9f87c572888008e6a7812e
filipe_nevola <filipe.nevola@tecsinapse.com.br>
2017-07-12 09:42:32 -0400

    formatting

--- client/main.js
+++ client/main.js
@@ -1,36 +1,42 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {Template} from 'meteor/templating';
+import {ReactiveVar} from 'meteor/reactive-var';
 import SubtitlesLines from '/model/SubtitlesLines';
 import SubtitlesWords from '/model/SubtitlesWords';
 
 import './main.html';
 
-Template.startSubtitle.onCreated(function () {
-  this.timer = new ReactiveVar(0);
+Template.startTimer.onCreated(function () {
+    this.timer = new ReactiveVar(0);
 });
 
 const getCurrentSubtitleLine = () => {
-  const subtitleLine = SubtitlesLines.findOne({secondStart: Template.instance().timer.get()})
-  return subtitleLine ? subtitleLine : '';
+    const subtitleLine = SubtitlesLines.findOne({secondStart: Template.instance().timer.get()})
+    return subtitleLine ? subtitleLine : '';
 };
 
-Template.startSubtitle.helpers({
-  subtitleLine() {
-    return getCurrentSubtitleLine();
-  },
-    subtitleWord() {
-      const words = getCurrentSubtitleLine().text.split(' ').map(word => word.toLowerCase());
-      console.log(words);
-      return SubtitlesWords.find({word: {$in: words} }, {sort: {frequency: 1}, limit: 1}).fetch()[0];
+Template.startTimer.helpers({
+    subtitleLine() {
+        return getCurrentSubtitleLine();
     },
+    subtitleWord() {
+        const words = getCurrentSubtitleLine().text.split(' ').map(word => word.toLowerCase());
+        console.log(words);
+        return SubtitlesWords.find({word: {$in: words}}, {sort: {frequency: 1}, limit: 1}).fetch()[0];
+    },
+    timer() {
+        return Template.instance().timer.get();
+    }
+});
+Template.startTimer.onCreated(function () {
+    this.timer = new ReactiveVar(0);
 });
 
-Template.startSubtitle.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    setInterval(() => {
-    	instance.timer.set(instance.timer.get() + 1);
-    	console.log(instance.timer.get());
-    }, 1000);
-  },
+Template.startTimer.events({
+    'click button'(event, instance) {
+        // increment the counter when button is clicked
+        setInterval(() => {
+            instance.timer.set(instance.timer.get() + 1);
+            console.log(instance.timer.get());
+        }, 1000);
+    },
 });
--- server/main.js
+++ server/main.js
@@ -1,52 +1,52 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import fs from 'fs';
 import SubtitlesLines from '/model/SubtitlesLines';
 import SubtitlesWords from '/model/SubtitlesWords';
 
 Meteor.startup(() => {
-      const data = fs.readFileSync("/home/tgmarinho/Documentos/workspace/soobdoo/subtitles/New.Girl.S01E01-2.srt", "utf8")
+    const data = fs.readFileSync("/home/filipe/Documents/codeftw/ws/soobdoo/subtitles/New.Girl.S01E01-2.srt", "utf8")
     const lines = data.split('\n');
 
     let i = 0;
-    while(i < lines.length) {
+    while (i < lines.length) {
         const subtitleNumber = parseInt(lines[i++].trim());
-        if(!subtitleNumber) {
-        	break;
+        if (!subtitleNumber) {
+            break;
         }
         const [timeStart, timeEnd] = lines[i++].split('-->');
         let text = lines[i++];
 
-        while(i < lines.length && lines[i].trim() !== '') {
+        while (i < lines.length && lines[i].trim() !== '') {
             text += lines[i];
             ++i;
         }
         ++i;
-		let secondStart = timeStart.split(':')[2].split(',')[0];
-		secondStart = secondStart.indexOf('0') !== -1 ? secondStart.substring(1) : secondStart;
-		secondStart = parseInt(secondStart);
+        let secondStart = timeStart.split(':')[2].split(',')[0];
+        secondStart = secondStart.indexOf('0') !== -1 ? secondStart.substring(1) : secondStart;
+        secondStart = parseInt(secondStart);
 
-    text = text.replace('\r', '');
+        text = text.replace('\r', '');
         text = text.replace(',', '');
-            text = text.replace('.', '');
-                text = text.replace('-', '');
+        text = text.replace('.', '');
+        text = text.replace('-', '');
         //TODO verificar qual eh a legenda
         const result = SubtitlesLines.upsert({subtitleNumber},
-        {
-        	subtitleNumber,
-        	timeStart,
-        	secondStart,
-        	timeEnd,
-        	text,
-        });
+            {
+                subtitleNumber,
+                timeStart,
+                secondStart,
+                timeEnd,
+                text,
+            });
 
         text.split(' ').forEach(word => {
-          word = word.toLowerCase();
-          const subtitleWord = SubtitlesWords.findOne({word});
-          if(subtitleWord) {
-            SubtitlesWords.update({word}, {$inc: {frequency: 1}});
-          } else {
-            SubtitlesWords.insert({word, frequency: 1});
-          }
+            word = word.toLowerCase();
+            const subtitleWord = SubtitlesWords.findOne({word});
+            if (subtitleWord) {
+                SubtitlesWords.update({word}, {$inc: {frequency: 1}});
+            } else {
+                SubtitlesWords.insert({word, frequency: 1});
+            }
         });
     }
 });
