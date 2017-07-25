289067520be948d5085da41336e7011c6689c297
ruither.borba <ruither.borba@tecsinapse.com.br>
2017-07-25 20:55:51 -0300

    adicionando controle de play/pause da legenda

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import {Template} from 'meteor/templating';
 import {ReactiveVar} from 'meteor/reactive-var';
 import SubtitlesLines from '/model/SubtitlesLines';
 import SubtitlesWords from '/model/SubtitlesWords';
+import TimeConverter from '/imports/core/TimeConverter';
 
 import './main.html';
 
@@ -27,20 +28,24 @@ Template.startTimer.helpers({
             return '';
 
         const words = currentSubtitleLine.split(' ').map(word => word.toLowerCase());
-        console.log(words);
+        // console.log(words);
         return SubtitlesWords.find({word: {$in: words}}, {sort: {frequency: 1}, limit: 1}).fetch()[0];
     },
     timer() {
-        return Template.instance().timer.get();
+        return TimeConverter.fromMilliseconsToTime(Template.instance().timer.get());
     }
 });
 
 Template.startTimer.events({
-    'click button'(event, instance) {
-        // increment the counter when button is clicked
-        setInterval(() => {
-            instance.timer.set(instance.timer.get() + 1000);
-            console.log(instance.timer.get());
-        }, 1000);
+    'click .js-play'(event, instance) {
+      // increment the counter when button is clicked
+      instance.playerHandler = Meteor.setInterval(() => {
+          instance.timer.set(instance.timer.get() + 1000);
+          console.log(instance.timer.get());
+      }, 1000);
     },
+
+    'click .js-pause'(event, instance) {
+      Meteor.clearInterval(instance.playerHandler);
+    }
 });
--- imports/core/TimeConverter.js
+++ imports/core/TimeConverter.js
@@ -8,6 +8,24 @@ const TimeConverter = {
         let [seconds, milliseconds] = secondsWithMilliseconds.split(',');
 
         return parseInt(milliseconds) + seconds*1000 + minutes*60*1000 + hours*60*60*1000;
+    },
+
+    fromMilliseconsToTime(milliseconds) {
+      var hours = milliseconds / (1000*60*60);
+      var absoluteHours = Math.floor(hours);
+      var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
+
+      //Get remainder from hours and convert to minutes
+      var minutes = (hours - absoluteHours) * 60;
+      var absoluteMinutes = Math.floor(minutes);
+      var m = absoluteMinutes > 9 ? absoluteMinutes : '0' +  absoluteMinutes;
+
+      //Get remainder from minutes and convert to seconds
+      var seconds = (minutes - absoluteMinutes) * 60;
+      var absoluteSeconds = Math.floor(seconds);
+      var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
+      console.log(h + ':' + m + ':' + s);
+      return h + ':' + m + ':' + s;
     }
 };
 
